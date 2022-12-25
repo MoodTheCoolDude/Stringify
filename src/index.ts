9591b892f5f7d5b8ea3730206024522c7f14792f
MoodTheCoolDude <97322519+TheUnblockedLabs@users.noreply.github.com>
2022-12-24 19:45:59 -0500

    remove testing code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import { insertNewLines } from "./functions/insertNewLines";
-
 export { join } from "./functions/join";
 export { randomString } from "./functions/randomString";
 export { reverse } from "./functions/reverse";
@@ -11,10 +9,3 @@ export { isAllCapital } from "./functions/isAllCapital";
 export { isAllLowerCase } from "./functions/isAllLowerCase";
 export { isCapitalized } from "./functions/isCapitalized";
 export { insertNewLines } from "./functions/insertNewLines";
-const longString =
-	"The insertNewLines function is used to insert line breaks in a long string to improve readability and understanding. It can be used to format long strings and make them easier to work with.";
-// console.log(insertNewLines(longString, 50, "hyphenate"));
-// console.log(insertNewLines(longString, 50, "hyphenate", "!!!"));
-// console.log(insertNewLines(longString, 50, "moveToBegin"));
-// console.log(insertNewLines(longString, 50, "moveToEnd"));
-console.log(insertNewLines(longString, 50, "none"));
